cb242f611ab47729b3e9eb94ca97ad0feab75f76
ZycsCF <zycsnitroseller@gmail.com>
2023-03-08 20:08:04 +0200

    Upload

---
+++ esx_zombiesystem/config.lua
@@ -0,0 +1,198 @@
+Config = {}
+
+Config.SpawnZombie = 2500
+Config.MinSpawnDistance = 50
+Config.MaxSpawnDistance = 100
+Config.DespawnDistance = 500
+Config.ZombieDropLoot = true
+Config.ProbabilityWeaponLoot = 3 
+Config.ProbabilityMoneyLoot = 33 
+Config.ProbabilityItemLoot = 53 
+
+Config.SafeZone = true
+Config.SafeZoneRadioBlip = true
+
+Config.SafeZoneCoords = {
+
+	{x = 322.46, y = -597.27, z = 43.28, radio = 40}, -- PillBox Hospital
+	{x = -45.65, y = -1098.24, z = 26.42, radio = 40}, -- PDM Motorsports
+	{x = -350.74, y = -1567.49, z = 25.22, radio = 30}, -- Sell Resources Garbage Dump	
+	{x = 446.04, y = -993.56, z = 27.0, radio = 40}, -- MRPD
+	{x = 1443.86, y = 6348.47, z = 23.86, radio = 40}, -- Mount Chiliad Commune
+	{x = -454.3, y = 5996.25, z = 31.34, radio = 25}, -- Paleto PD
+	{x = 1769.44, y = 3326.63, z = 31.34, radio = 40}, -- FLYWHEELS
+
+}
+
+Config.WeaponLoot = {
+	"WEAPON_PISTOL",
+	"WEAPON_FLASHLIGHT",
+	"WEAPON_MACHETE",
+	"WEAPON_NIGHTSTICK",
+	"WEAPON_BAT",
+	"WEAPON_STUNGUN",
+	"WEAPON_MICROSMG",
+	"WEAPON_PUMPSHOTGUN",
+	"WEAPON_ASSAULTRIFLE",
+	"WEAPON_SPECIALCARBINE",
+	"WEAPON_SNIPERRIFLE",
+	"WEAPON_FIREWORK",
+	"WEAPON_GRENADE",
+	"WEAPON_BZGAS",
+	"WEAPON_FIREEXTINGUISHER",
+	"WEAPON_BALL",
+	"WEAPON_SMOKEGRENADE"
+}
+Config.ItemLoot = {
+	"yusuf",
+	"grip",
+	"flashlight",
+	"suppressor",
+	"binoculars",
+	"boxpistol",
+	"boxsmg",
+	"boxshot",
+	"boxrifle",
+	"boxflare",
+	"oxygenmask",
+	"repairkit",
+	"boxsniper",
+	"bulletproof",
+	"boxmg",
+	'bandage',
+	'bandage2',
+	'medikit',
+	'bruiser',
+	'bruiser2',
+	'bruiser3',
+	'brutus',
+	'brutus2',
+	'brutus3',
+	'deathbike',
+	'deathbike2',
+	'deathbike3',
+	'deviant',
+	'impaler',
+	'impaler2',
+	'impaler3',
+	'impaler4',
+	'imperator',
+	'imperator2',
+	'imperator3',
+	'issi4',
+	'issi5',
+	'issi6',
+	'monster3',
+	'monster4',
+	'monster5',
+	'slamvan4',
+	'slamvan5',
+	'slamvan6',
+	'tulip',
+	'vamos',
+	'zr380',
+	'zr3802',
+	'zr3803',
+	'dominator4',
+	'dominator5',
+	'dominator6',
+	'cerberus',
+	'cerberus2',
+	'cerberus3',
+	'scarab',
+	'scarab2',
+	'scarab3',
+}
+
+
+
+Config.ObjectDropLoot = true --false if you experience low performance of server
+Config.ObjectsLoot = { -- objects in here can be looted around the map
+	"prop_bin_01a",
+	"prop_bin_02a",
+	"prop_bin_05a",
+	"prop_bin_08a",
+	"prop_bin_08open",
+	"p_dumpster_t",
+	"prop_cs_dumpster_01a",
+	"prop_dumpster_01a",
+	"prop_dumpster_02a",
+	"prop_dumpster_02b",
+	"prop_dumpster_3a",
+	"prop_dumpster_4a",
+	"prop_dumpster_4b"
+}
+
+Config.ProbabilityWeaponLootObject = 2 
+Config.ProbabilityMoneyLootObject = 22 
+Config.ProbabilityItemLootObject = 42 
+
+Config.WeaponLootObject ={
+	"WEAPON_PISTOL",
+	"WEAPON_FLASHLIGHT",
+	"WEAPON_MACHETE",
+	"WEAPON_NIGHTSTICK",
+	"WEAPON_BAT",
+	"WEAPON_STUNGUN",
+	"WEAPON_MICROSMG",
+	"WEAPON_PUMPSHOTGUN",
+	"WEAPON_GRENADE",
+	"WEAPON_BZGAS",
+	"WEAPON_FIREEXTINGUISHER",
+	"WEAPON_BALL",
+	"WEAPON_SMOKEGRENADE"
+}
+Config.ItemLootObject = {
+	'bandage',
+	'bandage2',
+	'medikit',
+	'bruiser',
+	'bruiser2',
+	'bruiser3',
+	'brutus',
+	'brutus2',
+	'brutus3',
+	'deathbike',
+	'deathbike2',
+	'deathbike3',
+	'deviant',
+	'impaler',
+	'impaler2',
+	'impaler3',
+	'impaler4',
+	'imperator',
+	'imperator2',
+	'imperator3',
+	'issi4',
+	'issi5',
+	'issi6',
+	'monster3',
+	'monster4',
+	'monster5',
+	'slamvan4',
+	'slamvan5',
+	'slamvan6',
+	'tulip',
+	'vamos',
+	'zr380',
+	'zr3802',
+	'zr3803',
+	'dominator4',
+	'dominator5',
+	'dominator6',
+	'cerberus',
+	'cerberus2',
+	'cerberus3',
+	'scarab',
+	'scarab2',
+	'scarab3',
+}
+
+
+
+Config.NoPeds = true
+Config.Blackout = false
+Config.MuteAmbience = true
+Config.NotHealthRecharge = true
+
+Config.Debug = false
